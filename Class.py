f3b5e9378732df73ea44593a6887dbe886dd679f
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-03-09 15:13:53 +0700

    ._.

--- Class.py
+++ Class.py
@@ -3,16 +3,16 @@ class Person:
     def __init__(self, name, age, ID, school, address):
         self.name = name
         self.age = age
-        self.ID = ID
+        self.__ID = ID
         self.school = school
-        self.address = address
+        self._address = address
 
     def introduce(self): 
         print(f"My name is {self.name}")
         print(f"I'm {self.age} years old")
-        print(f"My ID is {self.ID}")
+        print(f"My ID is {self._Person__ID}")
         print(f"I study at {self.school}")
-        print(f"I live in {self.address}")
+        print(f"I live in {self._address}")
 
     def greeting(self): 
         print(f"Hello, nice to meet you. My name is {self.name}, happy to see you")
@@ -23,3 +23,6 @@ person2 = Person("Marry", 25, 3742738, "KHXH & NV", "Vietnam")
 person1.greeting()
 print("---------------------------------------")
 person2.introduce()
+print("---------------------------------------")
+print(person1._Person__ID) #Truy cập biến private từ bên ngoài nhưng không khuyến khích
+#print(person1.__ID): Lỗi AttributeError (truy cập trực tiếp vào biến private sẽ bị lỗi)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+        # print(f"My ID is {self._Person__ID}")
+        # print(f"I study at {self.school}")
